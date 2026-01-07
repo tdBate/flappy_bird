7a3cac66ad4342fc3ddb969eeda6d8efada1d6e6
TorokD <td.zazsa@gmail.com>
2026-01-07 08:17:28 +0100

    Add project files.

---
+++ flappy_bird/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace flappy_bird
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
